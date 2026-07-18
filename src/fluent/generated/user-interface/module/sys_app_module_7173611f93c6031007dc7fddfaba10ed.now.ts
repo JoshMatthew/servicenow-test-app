9e26593cbfb55b70a424262283acabe42a68674e
System Administrator <admin@example.com>
2026-07-18 16:57:28 +0800

    add Pending Module for Training Order Module

---
+++ src/fluent/generated/user-interface/module/sys_app_module_7173611f93c6031007dc7fddfaba10ed.now.ts
@@ -0,0 +1,20 @@
+import { Record } from '@servicenow/sdk/core'
+
+Record({
+    $id: Now.ID['7173611f93c6031007dc7fddfaba10ed'],
+    table: 'sys_app_module',
+    data: {
+        active: true,
+        filter: 'state=-5^EQ',
+        link_type: 'LIST',
+        mobile_title: 'Pending',
+        mobile_view_name: 'Mobile',
+        name: 'u_training_order',
+        override_menu_roles: false,
+        require_confirmation: true,
+        sys_domain: 'global',
+        sys_domain_path: '/',
+        title: 'Pending',
+        uncancelable: false,
+    },
+})
